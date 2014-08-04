a374e633e92d31aad97330177f7fde06ec4257d6
Alexander Schneider <alexander.schneider@jankowfsky.com>
2014-08-04 17:45:06 +0200

    Refactor options definition

--- src/bashop/command.sh
+++ src/bashop/command.sh
@@ -21,57 +21,149 @@ bashop::command::show_help() {
 bashop::command::parse_arguments() {
   bashop::command::__check_dependencies
 
+  #TODO make local and store later global as read only
   declare -g -A args=()
 
-  # Get function agruments
-  local raw_arguments=("${@}")
+  #Get function agruments
+  local command_arguments=("${!1}")
+  local command_options=("${!2}")
+  local raw_arguments=("${!3}")
 
-  # Get definied options
-  local opt_required=()
-  local opt_map opt_type_map opt_default_arg
+  #Regex for options
+  local short_option_regex='(-[a-zA-Z]{1})([.]{3}){0,1}'
+  short_option_regex+='( ([\<][a-z]+[\>])| ([A-Z]+)){0,1}'
+  local long_option_regex='(--[a-zA-Z0-9\-]+)([.]{3}){0,1}'
+  long_option_regex+='(=([\<][a-z]+[\>])| ([A-Z]+)){0,1}'
+
+  #---- Command arguments ----
+
+  #Command regex
+  local command_arg_regex='(([\<][a-zA-Z0-9]+[\>])([.]{3}){0,1})'
+  local commmand_arg_requried_regex="^${command_arg_regex}$"
+  local commmand_arg_optional_regex="^\[${command_arg_regex}\]$"
+
+  #Get command arguments
+  for command_argument in ${command_arguments[@]}; do
+    if [[ ${command_argument} =~ ${commmand_arg_requried_regex} ]]; then
+      echo "Requried: ${command_argument}"
+    elif [[ ${command_argument} =~ ${commmand_arg_optional_regex} ]]; then
+      echo "Optional: ${command_argument}"
+    elif [[ ${command_argument} =~ ${short_option_regex} ]] || [[ ${command_argument} =~ ${long_option_regex} ]]; then
+      command_options+=( ${command_argument} )
+    fi
+  done
+
+
+  #---- Command options ----
+
+  #Get definied options
+  local opt_map opt_repeatable opt_default_arg
   declare -A opt_map=()
-  declare -A opt_type_map=()
+  declare -A opt_repeatable=()
   declare -A opt_default_arg=()
 
-  for opt in "${!BASHOP_COMMAND_OPTIONS[@]}"; do
-    local cur_opt=${opt}
+  #Short option regex
+  local option_regex+="^(${short_option_regex}[,]{0,1}[ ]{0,1}){0,1}"
 
+  #Long option regex
+  option_regex+="(${long_option_regex}){0,1}"
+
+  #Check for default value
+  option_regex+='([^\[]*)(\[default: ([a-zA-Z0-9]+)\]){0,1}.*$'
+
+  for opt in "${command_options[@]}"; do
     # Check if option is valid
-    if ! [[ ${cur_opt} =~ ^([a-z]{1}\|){0,1}([a-zA-Z0-9\-]+)([?:+]{1})(=\<([^:]+):{0,1}([a-zA-Z0-9\-]*)\>){0,1}$ ]]; then
-      bashop::logger::framework_error "Wrong pattern for option '${cur_opt}'"
+    if ! [[ ${opt} =~ ${option_regex} ]]; then
+      bashop::logger::framework_error "Wrong pattern for option '${opt}'."
       exit 1
     fi
 
-    local short_opt_name=${BASH_REMATCH[1]//\|/}
-    local full_opt_name="--${BASH_REMATCH[2]}"
-    local opt_type=${BASH_REMATCH[3]}
-    local has_arguments=${BASH_REMATCH[5]}
-    local default_arg=${BASH_REMATCH[6]}
+    local p_opts
+    declare -A p_opts=()
 
-    # Get option names
-    if [[ -n "${short_opt_name}" ]]; then
-      opt_map["-${short_opt_name}"]=${full_opt_name}
+    #Get short options name and agrument name
+    p_opts["short_opt_name"]=${BASH_REMATCH[2]}
+    p_opts["short_opt_repeatable"]=${BASH_REMATCH[3]}
+    p_opts["short_opt_arg"]=''
+
+    if [[ -n "${BASH_REMATCH[5]}" ]]; then
+      p_opts["short_opt_arg"]=${BASH_REMATCH[5]}
+    elif [[ -n "${BASH_REMATCH[6]}" ]]; then
+      p_opts["short_opt_arg"]=${BASH_REMATCH[6]}
     fi
 
-    opt_map[${full_opt_name}]=${full_opt_name}
+    #Get long options name and argument name
+    p_opts["long_opt_name"]=${BASH_REMATCH[8]}
+    p_opts["long_opt_repeatable"]=${BASH_REMATCH[9]}
+    p_opts["long_opt_arg"]=''
 
-    # Get option type
-    if [[ ${opt_type} == ":" ]]; then
-      opt_required+=(${full_opt_name})
+    if [[ -n "${BASH_REMATCH[11]}" ]]; then
+      p_opts["long_opt_arg"]=${BASH_REMATCH[11]}
+    elif [[ -n "${BASH_REMATCH[12]}" ]]; then
+      p_opts["long_opt_arg"]=${BASH_REMATCH[12]}
     fi
 
-    opt_type_map[${full_opt_name}]=${opt_type}
+    #Get default option argument
+    local default_option_arg=''
 
-    # Set default arg
-    if [[ -n "${has_arguments}" ]]; then
-      if [[ -n "${default_arg}" ]]; then
-        opt_default_arg[${full_opt_name}]=${default_arg}
+    if [[ -n "${BASH_REMATCH[15]}" ]]; then
+      default_option_arg=${BASH_REMATCH[15]}
+    fi
+
+    local type_name=''
+    local cur_opt=''
+
+    #Set options
+    if [[ -n "${p_opts["short_opt_name"]}" ]] && [[ -n "${p_opts["long_opt_name"]}" ]]; then
+      #Repeatable
+      if ( [[ -n "${p_opts["short_opt_repeatable"]}" ]] && ! [[ -n "${p_opts["long_opt_repeatable"]}" ]] ) ||
+         ( ! [[ -n "${p_opts["short_opt_repeatable"]}" ]] && [[ -n "${p_opts["long_opt_repeatable"]}" ]] )
+      then
+        bashop::logger::framework_error "One of the option of '${opt}' is repeatable so both must be repeatable."
+        exit 1
+      fi
+
+      #Arguments
+      if ( [[ -n "${p_opts["short_opt_arg"]}" ]] && ! [[ -n "${p_opts["long_opt_arg"]}" ]] ) ||
+         ( ! [[ -n "${p_opts["short_opt_arg"]}" ]] && [[ -n "${p_opts["long_opt_arg"]}" ]] )
+      then
+        bashop::logger::framework_error "One of the option of '${opt}' accepts an argument so both must accept one."
+        exit 1
+      fi
+
+      opt_map["${p_opts["short_opt_name"]}"]=${p_opts["long_opt_name"]}
+      type_name='long'
+      cur_opt=${p_opts["long_opt_name"]}
+    elif [[ -n "${p_opts["short_opt_name"]}" ]]; then
+      type_name='short'
+      cur_opt=${p_opts["short_opt_name"]}
+      opt_map[${cur_opt}]=${cur_opt}
+    elif [[ -n "${p_opts["long_opt_name"]}" ]]; then
+      type_name='long'
+      cur_opt=${p_opts["long_opt_name"]}
+      opt_map[${cur_opt}]=${cur_opt}
+    else
+      bashop::logger::framework_error "Wrong pattern for option '${opt}'."
+      exit 1
+    fi
+
+    if [[ -n "${p_opts["${type_name}_opt_repeatable"]}" ]]; then
+      opt_repeatable[${cur_opt}]=true
+    else
+      opt_repeatable[${cur_opt}]=false
+    fi
+
+    if [[ -n "${p_opts["${type_name}_opt_arg"]}" ]]; then
+      if [[ -n "${default_option_arg}" ]]; then
+        opt_default_arg[${cur_opt}]=${default_option_arg}
       else
-        opt_default_arg[${full_opt_name}]=false
+        opt_default_arg[${cur_opt}]=false
       fi
     fi
   done
 
+
+  #---- Parese arguments ----
 
   # Iterate over the raw argumgents
   local no_commands=${#_BASHOP_COMMAND[@]}
@@ -105,7 +197,7 @@ bashop::command::parse_arguments() {
       current_arg=${arg}
       local is_multiple_opt=false
 
-      if [[ ${opt_type_map[${current_arg}]} == '+' ]]; then
+      if [[ ${opt_repeatable[${current_arg}]} ]]; then
         is_multiple_opt=true
       fi
 
@@ -162,13 +254,5 @@ bashop::command::parse_arguments() {
     fi
 
     counter=$((counter + 1))
-  done
-
-  # Check for missing required vars
-  for req_opt in "${opt_required[@]}"; do
-    if !(bashop::utils::key_exists ${req_opt} args); then
-      bashop::logger::error "Option ${req_opt} is required"
-      exit 1
-    fi
   done
 }
