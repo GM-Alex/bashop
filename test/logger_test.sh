a59e17d44b2a7e2dddafdb2860e9feb11491fedf
Alexander Schneider <alexander.schneider@jankowfsky.com>
2014-08-11 11:10:57 +0200

    Add tests for the logger

---
+++ test/logger_test.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source ${DIR}/../src/bashop/logger.sh
+source ${DIR}/../src/bashop/utils.sh
+
+function check_echo() {
+  #Should be 'test\n' but last \n will be removed from bash, so that should be also ok
+  local string=$(bashop::logger::echo "test")
+  assertion__equal $'test' "${string}"
+
+  string=$(bashop::logger::echo "test" "!")
+  assertion__equal "test!" "${string}"
+
+  string=$(bashop::logger::echo "test" false)
+  assertion__equal "test" "${string}"
+}
+
+function check_info() {
+  local test_string=$'\033[00;34mInfo: test\n\033[0m'
+  local fnc_string=$(bashop::logger::info "test")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;34mInfo: test!\033[0m'
+  fnc_string=$(bashop::logger::info "test" "!")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;34mInfo: test\033[0m'
+  fnc_string=$(bashop::logger::info "test" false)
+  assertion__equal "${test_string}" "${fnc_string}"
+}
+
+function check_user() {
+  local test_string=$'\033[00;33mtest\n\033[0m'
+  local fnc_string=$(bashop::logger::user "test")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;33mtest!\033[0m'
+  fnc_string=$(bashop::logger::user "test" "!")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;33mtest\033[0m'
+  fnc_string=$(bashop::logger::user "test" false)
+  assertion__equal "${test_string}" "${fnc_string}"
+}
+
+function check_success() {
+  local test_string=$'\033[00;32mtest\n\033[0m'
+  local fnc_string=$(bashop::logger::success "test")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;32mtest!\033[0m'
+  fnc_string=$(bashop::logger::success "test" "!")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;32mtest\033[0m'
+  fnc_string=$(bashop::logger::success "test" false)
+  assertion__equal "${test_string}" "${fnc_string}"
+}
+
+function check_error() {
+  local test_string=$'\033[00;31mError: test\n\033[0m'
+  local fnc_string=$(bashop::logger::error "test")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;31mError: test!\033[0m'
+  fnc_string=$(bashop::logger::error "test" "!")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;31mError: test\033[0m'
+  fnc_string=$(bashop::logger::error "test" false)
+  assertion__equal "${test_string}" "${fnc_string}"
+}
+
+function check_verbose() {
+  local test_string=$'\033[00;34mtest\n\033[0m'
+  local fnc_string=$(bashop::logger::verbose "test")
+  assertion__string_empty "${fnc_string}"
+
+  declare -g _BASHOP_VERBOSE=false
+  fnc_string=$(bashop::logger::verbose "test")
+  assertion__string_empty "${fnc_string}"
+
+  _BASHOP_VERBOSE=true
+  fnc_string=$(bashop::logger::verbose "test")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;34mtest!\033[0m'
+  fnc_string=$(bashop::logger::verbose "test" "!")
+  assertion__equal "${test_string}" "${fnc_string}"
+
+  test_string=$'\033[00;34mtest\033[0m'
+  fnc_string=$(bashop::logger::verbose "test" false)
+  assertion__equal "${test_string}" "${fnc_string}"
+}
+
+function check_framework_error() {
+  local fnc_string=$(bashop::logger::framework_error "test")
+
+  assertion__string_contains "${fnc_string}" $'FRAMEWORK ERROR: test\n'
+  assertion__string_contains "${fnc_string}" $'\033[00;31m'
+  assertion__string_contains "${fnc_string}" $'\033[0m'
+}
+
+function check_help_formater() {
+  mock__make_function_do_nothing "bashop::logger::framework_error"
+
+  #Should be '  func   desc1\n  func2  desc2\n' but last '\n' will be removed from bash, so that should be also ok
+  local test_string=$'  func   desc1\n  func2  desc2'
+  local array=(
+    "func  desc1"
+    "func2  desc2"
+  )
+
+  local fnc_string=$(bashop::logger::help_formater array[@])
+  assertion__equal "${test_string}" "${fnc_string}"
+
+
+  array+=( "wrongfunc wrong_desc" )
+
+  (bashop::logger::help_formater array[@])
+  assertion__status_code_is_failure $?
+}
